a17fc828791462e09c0fb430a6f447e4f9616b4b
achiefa <amedeochiefa@gmail.com>
2024-12-16 12:50:29 +0100

    Minor adjustments in utilities

--- Experiments/Integrated_solution/nnpdf/model_utils.py
+++ Experiments/Integrated_solution/nnpdf/model_utils.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
+from multiprocessing import Pool
 import numpy as np
+from functools import partial
+from tensorflow.keras import ops
 
 class ComputeObservable(tf.keras.layers.Layer):
     def __init__(self, fk_dict, **kwargs):
@@ -35,7 +38,28 @@ class ComputeObservable(tf.keras.layers.Layer):
         for exp, fk  in self.A.items():
           g[exp] = tf.einsum('Iia, ia -> I ', fk, f_pred)
         return g
+
+    def get_config(self):
+      config = super().get_config()
+      config.update({
+        'A': self.A 
+      })
+      return config
     
+
+class MyDense(tf.keras.layers.Dense):
+  def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+  def call(self, inputs):
+        x = ops.matmul(inputs, self.kernel)
+        x = ops.divide(x, ops.sqrt(self.kernel.shape[0]))
+        if self.bias is not None:
+            x = ops.add(x, self.bias)
+        if self.activation is not None:
+            x = self.activation(x)
+        return x
+
 
 
 def generate_sequential_model(outputs=1, 
@@ -44,6 +68,7 @@ def generate_sequential_model(outputs=1,
                    units=[100,100],
                    seed=0,
                    predictions=False,
+                   dtype='float32',
                    **kwargs):
   """
   Create a tensorflow sequential model where all intermediate layers have the same size
@@ -66,7 +91,9 @@ def generate_sequential_model(outputs=1,
   if len(units) != nlayers:
       raise Exception("The length of units must match the number of layers.")
   
-  if kwargs.get('kernel_initializer'):
+  if kwargs.get('kernel_initializer') == 'RandomNormal':
+      kernel_initializer = tf.keras.initializers.RandomNormal
+  elif kwargs.get('kernel_initializer'):
       kernel_initializer = kwargs['kernel_initializer']
   else:
       kernel_initializer = tf.keras.initializers.HeNormal
@@ -90,17 +117,17 @@ def generate_sequential_model(outputs=1,
   
   model = tf.keras.models.Sequential(name=name)
   if input_layer is not None:
-      model.add(tf.keras.layers.InputLayer(shape=(50,),name='input'))
+      model.add(tf.keras.layers.InputLayer(shape=(50,),name='input',  dtype=dtype))
   for layer in range(nlayers):
-      model.add(tf.keras.layers.Dense(units[layer], 
+      model.add(MyDense(units[layer], 
                                       activation=activation_list[layer],
-                                      kernel_initializer=kernel_initializer(seed=seed - layer),
-                                      ),
+                                      kernel_initializer=kernel_initializer(mean=0., stddev=1, seed=seed - layer),
+                                      dtype=dtype),
       )
-  model.add(tf.keras.layers.Dense(outputs, 
+  model.add(MyDense(outputs, 
                                   activation=output_func, 
-                                  kernel_initializer=kernel_initializer(seed=seed - nlayers)
-                                  ))
+                                  kernel_initializer=kernel_initializer(mean=0., stddev=1, seed=seed - layer),
+                                  dtype=dtype))
   if predictions:  
     model.add(ComputeObservable(kwargs.get('fk_table_dict')))
 
@@ -118,7 +145,7 @@ def round_float32(value, ref_value, tol_magnitude=1.e-6):
     return value
   
 
-def compute_ntk(model, input, only_diagonal=True, round_to_zero=False):
+def compute_ntk(model, input, only_diagonal=False):
   """
   Copmute the Neural Tanget Kernel (NTK) of a Sequential model constructed
   with the Keras API.
@@ -147,6 +174,8 @@ def compute_ntk(model, input, only_diagonal=True, round_to_zero=False):
   ntk = tf.zeros((batch_size, n_outputs, batch_size, n_outputs))
   for jac in jacobian:
       prod = np.prod(jac.shape[2:])
+      # Somehow einsum wants float32
+      jac = tf.cast(jac, dtype=tf.float32)
       jac_concat = tf.reshape(jac, (jac.shape[0], jac.shape[1], prod)) 
       ntk += tf.einsum('iap,jbp->iajb', jac_concat, jac_concat)
 
@@ -156,27 +185,145 @@ def compute_ntk(model, input, only_diagonal=True, round_to_zero=False):
         ntk_diaogonal[:,f,:,f] = ntk[:,f,:,f]    
       ntk = ntk_diaogonal
   
-  if round_to_zero:
-      # Set the small eigenvalues to zero for round-off.
-      # Then reconstruct the original matrix 
+  # Deprecated
+  # if round_to_zero:
+  #     # Set the small eigenvalues to zero for round-off.
+  #     # Then reconstruct the original matrix 
 
-      # First, flatten the ntk into a matrix
-      oldshape = ntk.shape
-      prod = 1
-      for k in oldshape[2:]:
-        prod *= k
-      ntk_flatten = ntk.reshape(prod, -1)
+  #     # First, flatten the ntk into a matrix
+  #     oldshape = np.array(ntk.shape)
+  #     prod = 1
+  #     for k in oldshape[2:]:
+  #       prod *= k
+  #     try:
+  #        ntk = ntk.numpy()
+  #        ntk_flatten = ntk.reshape(prod, -1)
+  #     except AttributeError:
+  #       ntk_flatten = ntk.reshape(prod, -1)
 
-      # Compute eigenvalues and eigenfunction of the flatten NTK.
-      # Then rouond to zero
-      eigvals, eigvecs = np.linalg.eig(ntk_flatten)
+  #     # Compute eigenvalues and eigenfunction of the flatten NTK.
+  #     # Then rouond to zero
+  #     eigvals, eigvecs = np.linalg.eig(ntk_flatten)
 
-      # Forget about the imaginary part
-      eigvals = eigvals.real
-      eigvecs = eigvecs.real
-      for i in range(eigvals.size):
-          eigvals[i] = round_float32(eigvals[i], np.sort(eigvals)[::-1][0])
-      reconstructed = np.dot(eigvecs * eigvals, eigvecs.T)
-      ntk = reconstructed.reshape(oldshape)
+  #     # Forget about the imaginary part
+  #     eigvals = eigvals.real
+  #     eigvecs = eigvecs.real
+  #     for i in range(eigvals.size):
+  #         eigvals[i] = round_float32(eigvals[i], np.sort(eigvals)[::-1][0])
+  #     reconstructed = np.dot(eigvecs * eigvals, eigvecs.T)
+  #     ntk = reconstructed.reshape(oldshape)
   
   return ntk
+
+
+
+
+from validphys.theorycovariance.output import matrix_plot_labels
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+def plot_covmat_heatmap(covmat, title):
+    """Matrix plot of a covariance matrix."""
+    df = covmat
+
+    matrix = df.values
+    fig, ax = plt.subplots(figsize=(15, 15))
+    matrixplot = ax.matshow(
+        matrix,
+        cmap=cm.Spectral_r,
+        norm=mcolors.SymLogNorm(
+            linthresh=0.00001, linscale=1, vmin=-matrix.max(), vmax=matrix.max()
+        ),
+    )
+
+    # create an axes on the right side of ax. The width of cax will be 5%
+    # of ax and the padding between cax and ax will be fixed at 0.05 inch.
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.5)
+
+    cbar = fig.colorbar(matrixplot, cax=cax)
+    cbar.set_label(label=r"$\widetilde{P}$", fontsize=20)
+    cbar.ax.tick_params(labelsize=20)
+    ax.set_title(title, fontsize=25)
+    ticklocs, ticklabels, startlocs = matrix_plot_labels(df)
+    ax.set_xticks(ticklocs)
+    ax.set_xticklabels(ticklabels, rotation=30, ha="right", fontsize=20)
+    ax.xaxis.tick_bottom()
+    ax.set_yticks(ticklocs)
+    ax.set_yticklabels(ticklabels, fontsize=20)
+    
+    # Shift startlocs elements 0.5 to left so lines are between indexes
+    startlocs_lines = [x - 0.5 for x in startlocs]
+    ax.vlines(startlocs_lines, -0.5, len(matrix) - 0.5, linestyles="dashed")
+    ax.hlines(startlocs_lines, -0.5, len(matrix) - 0.5, linestyles="dashed")
+    ax.margins(x=0, y=0)
+    return fig
+
+
+def train_network(model, 
+                 optimizer, 
+                 input_model, 
+                 Cinv, 
+                 data, 
+                 show_log=False, 
+                 ntk_round_to_zero=False, 
+                 ntk_only_diagonal=False, 
+                 tol=1.e-8):
+  X = input_model
+  predictions_in_time = []
+  pdfs_in_time = []
+  NTK_in_time = []
+
+  saved_steps = []
+  step = 0
+  loss_pre = 0
+  while True:  # Number of epochs/iterations
+    # Save data
+    if step % 100 == 0:
+      pdf_from_model = tf.keras.models.Sequential(model.layers[:-1])
+      
+      NTK = compute_ntk(pdf_from_model, X.numpy(), round_to_zero=ntk_round_to_zero, only_diagonal=ntk_only_diagonal)
+      NTK = np.array(NTK)
+      prod = 1
+      oldshape = NTK.shape
+      for k in oldshape[2:]:
+          prod *= k
+      NTK = NTK.reshape(prod,-1)
+
+      pdfs_in_time.append(pdf_from_model(X))
+      NTK_in_time.append(NTK)
+      saved_steps.append(step)
+
+    with tf.GradientTape(persistent=False) as tape:
+        # Forward pass: Compute predictions
+        predictions = model(X)
+        #if step % 100 == 0:
+        predictions_in_time.append(predictions)
+
+        loss = 0
+        for exp, pred in predictions.items():
+          Cinv_exp = tf.convert_to_tensor(Cinv.xs(level="dataset", key=exp).T.xs(level="dataset", key=exp).to_numpy(), name=f'Cinv_{exp}', dtype='float32')
+          R = tf.convert_to_tensor(pred - np.array(data[exp]), name=f'residue_{exp}', dtype='float32')
+          Cinv_R = tf.linalg.matvec(Cinv_exp, R)
+          loss += 0.5 * tf.reduce_sum(tf.multiply(R, Cinv_R))
+        
+        if step == 0:
+          loss_pre = loss
+        else: 
+          if abs(loss - loss_pre)/loss_pre > tol and step != 0:
+             loss_pre = loss
+          elif abs(loss - loss_pre)/loss_pre < tol:
+             break
+
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+        if show_log:
+          if step % 100 == 0:
+            print('------------------------')
+            print(f"Step {step}, Loss: {loss.numpy()}")
+    step += 1
+  
+  return (NTK_in_time, predictions_in_time, pdfs_in_time, saved_steps)
+
+from concurrent.futures import ProcessPoolExecutor, as_completed
--- Experiments/Integrated_solution/nnpdf/utils.py
+++ Experiments/Integrated_solution/nnpdf/utils.py
@@ -14,3 +14,86 @@ XGRID = np.array([2.00000000e-07, 3.03430477e-07, 4.60350147e-07, 6.98420853e-07
        4.22166775e-01, 4.79898903e-01, 5.39757234e-01, 6.01472198e-01,
        6.64813948e-01, 7.29586844e-01, 7.95624252e-01, 8.62783932e-01,
        9.30944081e-01, 1.00000000e+00])
+
+
+def extract_independent_columns(matrix, DEBUG=False, **kwargs):
+    """
+    Extract a subset of linearly independent columns from the matrix, 
+    preserving the original order.
+    
+    Args:
+        matrix (numpy.ndarray): The input square matrix.
+        
+    Returns:
+        numpy.ndarray: A matrix containing only the independent columns.
+    """
+    independent_columns = []
+    current_matrix = np.empty((matrix.shape[0], 0), dtype=matrix.dtype)
+
+    # Starting condition
+    current_matrix = np.hstack([current_matrix, matrix[:, 0 : 0 + 1]])
+
+    for col_idx in range(1, matrix.shape[1]):
+        candidate_matrix = np.hstack([current_matrix, matrix[:, col_idx : col_idx + 1]])
+        if np.linalg.matrix_rank(candidate_matrix, **kwargs) > np.linalg.matrix_rank(current_matrix, **kwargs):
+            independent_columns.append(col_idx)
+            current_matrix = candidate_matrix
+
+    return matrix[:, independent_columns], independent_columns
+
+
+
+from n3fit.layers import FlavourToEvolution, FkRotation
+
+# Flavour map for NNPDF rotation
+flavour_map = [
+        {'fl': 'u'},
+        {'fl': 'ubar'},
+        {'fl': 'd'},
+        {'fl': 'dbar'},
+        {'fl': 's'},
+        {'fl': 'sbar'},
+        {'fl': 'c'},
+        {'fl': 'g'},
+    ]
+
+# Particle ID map
+PID_map = {
+  'd'    : 1,
+  'u'    : 2,
+  's'    : 3,
+  'c'    : 4,
+  'b'    : 5,
+  'dbar' : -1,
+  'ubar' : -2,
+  'sbar' : -3,
+  'cbar' : -4,
+  'bbar' : -5,
+  'g'    : 21
+}
+
+fk_ev_map = [
+  r'$\Sigma$',
+  r'$g$',
+  r'$V$',
+  r'$V_3$',
+  r'$V_8$',
+  r'$T_3$',
+  r'$T_8$',
+  r'$c^+$',
+  r'$V_{15}$',
+]
+
+def produce_R_flav_8_to_ev_9():
+  # Rotates from flavour basis to evolution basis.
+  # The evolution basis is [sng, g, v, v3, v8, t3, t8, cp, v15]
+  # (v_ev)_j = R_{ij} * (v_fl)_{i} ---> (v_ev) = R^T * v_fl
+  R_flav_8_to_ev_9_layer = FlavourToEvolution(flavour_map, "FLAVOUR")
+  return R_flav_8_to_ev_9_layer.rotation_matrix.numpy()   
+
+def produce_R_ev_9_to_flav_8():
+  R_flav_8_to_ev_9 = produce_R_flav_8_to_ev_9()
+  # Invert rotation matrix
+  RR_T = R_flav_8_to_ev_9 @ R_flav_8_to_ev_9.T
+  RR_inv = np.linalg.inv(RR_T)
+  return RR_inv @ R_flav_8_to_ev_9
